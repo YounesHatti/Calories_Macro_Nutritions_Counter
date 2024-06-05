8845515d64ce32629987a55bdf08be48d5e26937
YounesHatti <hattiyounes@gmail.com>
2024-06-06 00:44:08 +0100

    Update Calories Counter 02.py
    This version count the daily Fat, Carbs and Protein needed.

--- Calories Counter 00.py
+++ Calories Counter 00.py
@@ -1,17 +1,17 @@
-# Calories counter
+#Calories counter
 
-# inPut user data
 Age = input("Enter your age (in years): ")
 Height = input("Enter your height (in cm): ")
 Weight = input("Enter your weight(in kg): ")
 Sex = input("Enter M for Male, F for Female: ")
+Goal = input("Type 'G' if you want to gain weight, 'L' if you want to lose weight: ")
 
 # Convert variables into floats to use in the calculations.
+
 A = float(Age)
 H = float(Height)
 W = float(Weight)
 
-# Revised Harris-Benedict Equation to calculate BMR depends on the sex
 if Sex == "M":
     BMR = 88.362 + (13.397 * W) + (4.799 * H) - (5.677 * A)
 if Sex == "F":
@@ -20,8 +20,7 @@ if Sex == "F":
 print("Your Basic Metabolic Rate is: BMR = ", BMR, "Calories")
 print("BMR is the amount of energy expended per day at rest.")
 
-# TDEE depend on the activity
-print("""Chose the number coresponding to your activity level
+print("""
          1. Secondary: Little to no exercise
          2. Light: Exercise activities
          3. Moderate: at least exercise 3 times a week pluse medium work.
@@ -41,6 +40,36 @@ if Activity == "4":
 if Activity == "5":
     TDEE = BMR*1.9
 
-print("Your Total Daily Energy Expenditure is: TDEE = ", TDEE, "Calories")
+#Prot is Protein
+if Activity == 1:
+    Prot = W*1
+elif Goal == "G":
+    Prot = W*1.7
+elif Goal == "L":
+    Prot = W*2
+else:
+    Prot=0
 
+ProCal = Prot*4
+
+#Fat is necessary
+Fat = W
+FatCal = Fat*9
+
+#Carbs
+CarbCal = TDEE - FatCal - ProCal
+Carb = CarbCal/4
+
+print("Your Total Daily Energy Expenditure is: TDEE = ", TDEE, "Calories")
+print("Your daily needs of Carbs is", Carb, "g")
+print("Responsible for ", CarbCal, "Calories")
+print("Your daily needs of Protein is", Prot, "g")
+print("Responsible for ", ProCal, "Calories")
+print("Your daily needs of Fat is", Fat, "g")
+print("Responsible for ", FatCal, "Calories")
+print("""
+        To maintain your weight, you need to eat calories equal to your TDEE
+        To lose weight, you need to eat less calories than your TDEE (-300 cal recommanded)
+        To gain weight, you need to eat more calories than your TDEE (+300 cal recommanded)
+            """)
 input("press any key to exit app")
