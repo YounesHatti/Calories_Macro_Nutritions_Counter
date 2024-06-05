1e20b4e5dd0ca1294411194d60052e05c73a22c7
YounesHatti <hattiyounes@gmail.com>
2024-06-05 22:27:42 +0100

    Calories Counter 01.py
    Added TDEE

--- Calories Counter 00.py
+++ Calories Counter 00.py
@@ -20,5 +20,27 @@ if Sex == "F":
 print("Your Basic Metabolic Rate is: BMR = ", BMR, "Calories")
 print("BMR is the amount of energy expended per day at rest.")
 
-# Exit
-input("Press any key to exit")
+# TDEE depend on the activity
+print("""Chose the number coresponding to your activity level
+         1. Secondary: Little to no exercise
+         2. Light: Exercise activities
+         3. Moderate: at least exercise 3 times a week pluse medium work.
+         4. Active: at least exercise 5 times a week pluse hard work.
+         5. Extra active: very intence daily exercise or work.
+            """)
+Activity = input("Choose the number coresponding to your activity: ")
+
+if Activity == "1":
+    TDEE = BMR*1.2
+if Activity == "2":
+    TDEE = BMR*1.375
+if Activity == "3":
+    TDEE = BMR*1.55
+if Activity == "4":
+    TDEE = BMR*1.727
+if Activity == "5":
+    TDEE = BMR*1.9
+
+print("Your Total Daily Energy Expenditure is: TDEE = ", TDEE, "Calories")
+
+input("press any key to exit app")
